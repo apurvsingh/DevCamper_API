12f25eae21fcd0c9cd7e6538345d73e03828738e
Apurv_Singh <Apurv_Singh@Dell.com>
2019-11-14 17:18:50 +0530

    Connect to db

--- server.js
+++ server.js
@@ -1,17 +1,20 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const morgan = require("morgan");
-//Logger
-//const logger = require("./middleware/logger");
-
-//Route Files
-const bootcamps = require("./routes/bootcamps");
+const connectDb = require('./config/db');
 
 //Load env vars
 dotenv.config({
   path: "./config/config.env"
 });
+
+//Connect DB
+connectDb();
+
 const app = express();
+
+//Route Files
+const bootcamps = require("./routes/bootcamps");
 
 //Dev logging middleware
 if (process.env.NODE_ENV === "development") {
@@ -22,7 +25,16 @@ if (process.env.NODE_ENV === "development") {
 app.use("/api/v1/bootcamps", bootcamps);
 
 const PORT = process.env.PORT;
-app.listen(
+const server = app.listen(
   PORT,
   console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
 );
+
+//Handle unhandled promise rejection
+process.on('unhandledRejection', (err, promise)=>{
+  console.log(`Error: ${err.message}`);
+  //Close server and exit
+  server.close(()=>{
+    process.exit(1);
+  });
+})
